10b332209f852c46f9333ef8ac48fb922fcdddff
Yujie Xia <xiayjchn@gmail.com>
2016-11-25 19:06:51 +0800

    Number Theory

---
+++ 51nod/1333.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<stdint.h>
+using namespace std;
+const int N = 5000000;
+int prime[N + 1], notprime[N + 1],f[N+1];
+int64_t sum[N + 1];
+
+int main() {
+    notprime[1] = 1;
+    for (int i = 2;i <= N;i++) {
+        if (!notprime[i]) prime[++prime[0]] = i;
+        for (int j = 1;j <= prime[0] && i <= N / prime[j];j++) {
+            notprime[i*prime[j]] = 1;
+            if (i%prime[j] == 0) break;
+        }
+    }
+    for (int i = 2;i*i <= N;i++) {
+        for (int j = i;i*j <= N;j++) {
+            if (notprime[i + j - 1]) {
+                f[i*j] = 1;
+            }
+        }
+    }
+    for (int i = 1;i <= N;i++) {
+        if (f[i - 1] == 0 && notprime[i - 1])
+            sum[i] = sum[i - 1] + i;
+        else
+            sum[i] = sum[i - 1];
+    }
+    int T;
+    cin >> T;
+    while (T--) {
+        int L, R;
+        cin >> L >> R;
+        int64_t ans = sum[R] - sum[L - 1];
+        if (L <= 2) ans -= 2;
+        cout << ans << endl;
+    }
+    return 0;
+}
---
+++ 51nod/1372.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<algorithm>
+#include<stdint.h>
+#include<map>
+using namespace std;
+const int P = 1000000007;
+const int N = 100000;
+int prime[N + 1], f[N + 1], inv[N + 1];
+
+int power_mod(int a, int n) {
+    int r = 1;
+    while (n) {
+        if (n & 1) r = (int64_t)r*a%P;
+        a = (int64_t)a*a%P;
+        n >>= 1;
+    }
+    return r;
+}
+
+void calc(int l, int r) {
+    if (l > r) return;
+    for (int i = l;i <= r;i++) {
+        int x = i;
+        for (int j = 1;j <= prime[0];j++) {
+            if (x / prime[j] < prime[j]) break;
+            if (x%prime[j] == 0) {
+                while (x%prime[j] == 0) {
+                    x /= prime[j];f[j]++;
+                }
+            }
+        }
+        if (x > 1) {
+            x = lower_bound(prime + 1, prime + 1 + prime[0], x) - prime;
+            f[x]++;
+        }
+    }
+}
+
+int main() {
+    for (int i = 2;i <= N;i++) {
+        if (!prime[i]) prime[++prime[0]] = i;
+        for (int j = 1;j <= prime[0] && i <= N / prime[j];j++) {
+            prime[i*prime[j]] = 1;
+            if (i%prime[j] == 0) break;
+        }
+    }
+    inv[1] = 1;
+    for (int i = 2;i <= N;i++) {
+        inv[i] = (int64_t)inv[P%i] * (P - P / i) % P;
+    }
+    int ans[20];
+    for (int i = 0;i < 20;i++) {
+        ans[i] = 1;
+    }
+    map<int, int>mp;
+    int T;
+    cin >> T;
+    for (int i = 0;i < T;i++) {
+        int x;
+        cin >> x;
+        mp[x] = i;
+    }
+    int pre = 2;
+    for (auto x : mp) {
+        calc(pre + 1, x.first);
+        pre = x.first;
+        for (int i = 1;i <= prime[0];i++) {
+            if (prime[i] > pre) break;
+            if (f[i]) ans[x.second] = (int64_t)ans[x.second] *
+                power_mod(prime[i], f[i] - 1) % P*((f[i] + 1)*(prime[i]) - f[i]) % P;
+        }
+        ans[x.second] = (ans[x.second] + P) % P;
+    }
+    for (int i = 0;i < T;i++) {
+        cout << "Case #" << i + 1 << ": " << ans[i] << endl;
+    }
+    return 0;
+}
