2b5bc0621ccce7ad0d246c1809ff397f5c67fd76
Yujie Xia <xiayjchn@gmail.com>
2016-12-15 22:46:25 +0800

    rebuild repository

---
+++ 51nod/level4/1449.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int w,m;
+    cin>>w>>m;
+    bool flag=true;
+    while(m){
+        if(m%w==0) m/=w;
+        else if(m%w==1) m=(m-1)/w;
+        else if(m%w==w-1) m=(m+1)/w;
+        else{
+            flag=false;
+            break;
+        }
+    }
+    if(flag) puts("YES");
+    else puts("NO");
+    return 0;
+}
---
+++ 51nod/level5/1189.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+typedef long long ll;
+const int mod=1000000007;
+const int N=1000000;
+int prime[N+1];
+
+int main(){
+    int n;
+    cin>>n;
+    int ans=1;
+    for(int i=2;i<=n;i++){
+        if(!prime[i]){
+            prime[++prime[0]]=i;
+            int base=i,cnt=n/base*2;
+            while(base<=n/i){
+                base*=i;
+                cnt+=n/base*2;
+            }
+            ans=(ll)ans*(cnt+1)%mod;
+        }
+        for(int j=1;j<=prime[0]&&i<=n/prime[j];j++){
+            prime[i*prime[j]]=1;
+            if(i%prime[j]==0) break;
+        }
+    }
+    ans=(ll)(ans+1)*(mod+1)/2%mod;
+    cout<<ans<<endl;
+    return 0;
+}
